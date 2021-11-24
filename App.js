d462779e273448d0311d097119eb3c5532afbcce
ESLL3N <esllenescolar@gmail.com>
2021-11-24 12:26:15 +0000

    uso de estado

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Formulario from "./components/formulario";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aoba!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Formulario/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
